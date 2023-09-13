0a26b769fc190b82d20f5861b14f24dbd7cb8c23
Evokki <118923275+Evokki@users.noreply.github.com>
2023-09-13 14:27:41 +0300

    Luotu classLibrary Peleille

---
+++ WPFgambleLibrary/Blackjack.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFgambleLibrary
+{
+    public class Blackjack: Game 
+    {
+        public Deck deck;
+        public Blackjack()
+        {
+            deck = new Deck();
+            Start();
+        }
+        public override void Start()
+        {
+            throw new NotImplementedException();
+        }
+        public override void Stop()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
