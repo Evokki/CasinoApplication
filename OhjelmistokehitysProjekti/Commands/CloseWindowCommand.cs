cc92d9114f1ac86d4afab4e3b9665319c96c3252
Veeti Varmanen <vvarmanen@gmail.com>
2023-09-17 16:12:42 +0300

    Refactored code. Added Temporary Test Window

---
+++ OhjelmistokehitysProjekti/Commands/CloseWindowCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace OhjelmistokehitysProjekti.Commands
+{
+    public class CloseWindowCommand : ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+        private Action<object> _Execute { get; set; }
+        private Predicate<object> _CanExecute { get; set; }
+
+        public CloseWindowCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            _Execute = execute;
+            _CanExecute = canExecute;
+        }
+
+        bool ICommand.CanExecute(object? parameter)
+        {
+            return _CanExecute(parameter);
+        }
+
+        void ICommand.Execute(object? parameter)
+        {
+            _Execute(parameter);
+        }
+    }
+}
---
+++ OhjelmistokehitysProjekti/TestWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using OhjelmistokehitysProjekti.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace OhjelmistokehitysProjekti
+{
+    /// <summary>
+    /// Test window for learning and testing XAML functinalities
+    /// </summary>
+    public partial class TestWindow : Window
+    {
+        public TestWindow()
+        {
+            InitializeComponent();
+
+            MainViewModel MVM = new MainViewModel();
+            this.DataContext = MVM;
+        }
+    }
+}
