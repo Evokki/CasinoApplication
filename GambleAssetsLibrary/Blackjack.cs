9577f7a8ea2366eb647bca3d00878fc938e2ad1d
Veeti Varmanen <vvarmanen@gmail.com>
2023-09-17 16:20:03 +0300

    Refactored code. Added Temporary Test Window

---
+++ GambleAssetsLibrary/Blackjack.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GambleAssetsLibrary
+{
+    public class Blackjack: Game 
+    {
+        public Deck deck;
+        public Blackjack()
+        {
+            deck = new Deck();
+            StartGame();
+        }
+        public override void StartGame()
+        {
+            throw new NotImplementedException();
+        }
+        public override void EndGame()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
