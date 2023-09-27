6ed26a4fdce81ce1b5d4949ca99324bdc58c42ff
Aleksanteri22000 <aleksanteri22000@student.hamk.fi>
2023-09-27 20:29:48 +0300

    Jotain sekoilua lisätty pokeriin

--- GambleAssetsLibrary/Poker.cs
+++ GambleAssetsLibrary/Poker.cs
@@ -8,12 +8,24 @@ namespace GambleAssetsLibrary
 {
     public class Poker : Game
     {
-        public Deck Deck; // Tossa sun pakkas. Saat Card-objectin sielt käyttämäl GetCard funktiota eli Deck.GetCard()
-        //Jos haluut katella tota lisää ni se löytyy tuolt libraryst BaseClasses.cs
+        public Deck Deck; // Tossa sun pakkas. Saat Card-objectin sielt käyttämäl GetCard funktiota eli Deck.GetCard()//Jos haluut katella tota lisää ni se löytyy tuolt libraryst BaseClasses.cs
+        public List<Card> Playerhand;             
         public Poker(string S) : base(S)
         {
             Deck = new Deck();
+            Playerhand = new List<Card>
+            {
+                Deck.GetCard(), // 2 ensimmäistä korttia
+                Deck.GetCard(),
+
+                Deck.GetCard(), // kaksi korttia lisää mistä käyttäjä valitsee yhden
+                Deck.GetCard()
+            };
+
+
         }
+
+      
         public override void StartGame() // Tätä funktiota ei ees callata mistää ni älä välitä
         {
             Console.WriteLine("Slots start"); //Tää on vaa sen takii et softa toimii. Se throw new exception kaataa aina pelin ni korvasin vaa tällä.
